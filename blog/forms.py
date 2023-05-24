eb5419c059c39a4bdbb0b85d0cd791ae544cb40c
DuriKang <kooda21@naver.com>
2023-05-24 23:01:24 +0900

    20230524 아직 덜만들긴 했음

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Post
-
+from .models import Post,Comment
+#게시글 폼
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+#댓글 폼
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author','text',)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404,redirect
+from django.middleware import csrf
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from .models import Post,Comment
+from .forms import PostForm,CommentForm
 
 #게시글 리스트 로직
 def post_list(request):
@@ -11,7 +13,38 @@ def post_list(request):
 #게시글 상세보기 로직
 def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
-    return render(request, 'blog/post_detail.html',{'post':post})
+    comments = Comment.objects.filter(post = post)
+    return render(request, 'blog/post_detail.html',{'post':post,'comments':comments})
+
+# 댓글 기능 로직(댓글 리스트 보여주기)
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    csrf_token = csrf.get_token(request)  # CSRF 토큰 얻기
+    return render(request, 'blog/add_comment_to_post.html', {'form': form, 'csrf_token': csrf_token})
+
+# 댓글 관리 로직(댓글 승인)
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+# 댓글 관리 로직(댓글 삭제)
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail',pk=comment.post.pk)
 
 #게시글 생성 로직
 def post_create(request):
