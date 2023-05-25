44d5092f8aa69e3e1d1c6fbecaa079c57fdd857d
DuriKang <kooda21@naver.com>
2023-05-25 15:40:25 +0900

    메인 사이트 수정중

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -35,3 +36,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+    
